201b72fe46a1095ae056017386dd09a130027883
Jonathanleivag <email@jonathanleivag.cl>
2025-05-27 11:42:17 -0400

    feat: update trainer details and enhance layout in TrainersComponent

--- src/components/home/trainers.component.tsx
+++ src/components/home/trainers.component.tsx
@@ -3,7 +3,6 @@
 import { motion } from "framer-motion";
 import { FC } from "react";
 import Image from "next/image";
-import SocialSharedComponent from "../shared/social.shared.component";
 
 const TrainersComponent: FC = () => {
   return (
