f86a80bc96af6a2310670c8adc4f52a65cdf0df4
Jonathanleivag <email@jonathanleivag.cl>
2025-05-30 11:04:01 -0400

    feat: update middleware to handle redirection for authenticated users on login page

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,9 +11,14 @@ export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
   const isProtectedRoute = pathname.startsWith("/dashboard");
+  const isAuthPage = pathname === "/login";
 
   if ((!token || token.provider !== "credentials") && isProtectedRoute) {
     return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  if (token?.provider === "credentials" && isAuthPage) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
   }
 
   return NextResponse.next();
